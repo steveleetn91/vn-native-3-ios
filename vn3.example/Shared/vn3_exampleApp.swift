893752fd8f40558fce0abfc4f5dd7f389afa90a0
Hoang lee <hoanglee@Hoangs-MacBook-Air.local>
2022-01-11 20:51:07 +0700

    Add iOS Example

---
+++ vn3.example/Shared/vn3_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  vn3_exampleApp.swift
+//  Shared
+//
+//  Created by Hoang lee on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct vn3_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
